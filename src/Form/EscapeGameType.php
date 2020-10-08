d994edf7eda1d7d615fbb860e7fa5eadd800a517
laurinehrd <laurine.hrd@hotmail.fr>
2020-10-08 16:12:51 +0200

    add form

---
+++ src/Form/EscapeGameType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\EscapeGame;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EscapeGameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('duration')
+            ->add('dateTime')
+            ->add('numberMin')
+            ->add('numberMax')
+            ->add('private')
+            ->add('excerpt')
+            ->add('categoryName')
+            ->add('resumeText')
+            ->add('IntroductionText')
+            ->add('InstructionsText')
+            ->add('happyEndText')
+            ->add('gameOverText')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => EscapeGame::class,
+        ]);
+    }
+}
