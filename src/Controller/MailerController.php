424c8faa8bebb529119f6c077e1f410513116eb8
celia <celiabellod@icloud.com>
2020-10-09 15:16:52 +0200

    add mailer env

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -5,41 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
-use App\Entity\Participant;
-use App\Repository\ParticipantRepository;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class MailerController extends AbstractController
 {
     /**
      * @Route("/email")
      */
-    public function sendEmail(MailerInterface $mailer, ParticipantRepository $participantRepository)
+    public function sendEmail(MailerInterface $mailer)
     {
-        $participantEmails = $participantRepository->findAllEmail();
+        /*$participantEmails = $participantRepository->findAllEmail();
 
         foreach($participantEmails as $value){
             $email = $value;
-        }
+        }*/
 
-
-        /*$email = (new Email())
-            ->from('hello@example.com')
-            ->to('you@example.com')
+        $email = (new Email())
+            ->from('c.bellod@codeur.online')
+            ->to('acscape21@gmail.com')
             //->cc('cc@example.com')
             //->bcc('bcc@example.com')
             //->replyTo('fabien@example.com')
             //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+            ->subject('Inscription à un Escape Game')
+            ->html('<p>Vous êtes maintenant inscrit pour cet Escape Game pour le 14 juin 2020 à 13h30</p><p>Pour vous connectez à la platform vous devrez rentrer ce code : 1234</p>');
 
-        $mailer->send($email);*/
+        $mailer->send($email);
 
-        return $this->render('mailer/index.html.twig', [
-            'participantEmails' =>  $email,
-        ]);
     }
 }
